78ac9298f071dba4d9267f9a43878ac4b4263079
Claes Erik <claeserikjr@gmail.com>
2025-01-03 10:45:10 -0300

    forms and animation completed

--- lib/components.dart
+++ lib/components.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class TabsWeb extends StatefulWidget {
@@ -32,7 +34,7 @@ class _TabsWebState extends State<TabsWeb> {
         duration: const Duration(milliseconds: 100),
         curve: Curves.elasticIn,
         style: isSelected
-            ? GoogleFonts.oswald(
+            ? GoogleFonts.roboto(
                 shadows: [
                   Shadow(
                     color: widget.accentColor,
@@ -42,10 +44,10 @@ class _TabsWebState extends State<TabsWeb> {
                 fontSize: 25.0,
                 color: Colors.black,
                 decoration: TextDecoration.underline,
-                decorationThickness: 1,
+                decorationThickness: 1.0,
                 decorationColor: widget.accentColor,
               )
-            : GoogleFonts.oswald(color: Colors.black, fontSize: 23.0),
+            : GoogleFonts.roboto(color: Colors.black, fontSize: 23.0),
         child: Text(
           widget.title,
           style: GoogleFonts.oswald(
@@ -86,6 +88,143 @@ class Sans extends StatelessWidget {
       text,
       style: GoogleFonts.openSans(
         fontSize: size,
+      ),
+    );
+  }
+}
+
+class TextForm extends StatelessWidget {
+  final accentColor;
+  final heading;
+  final width;
+  final hintext;
+  final maxLines;
+
+  const TextForm(
+      {super.key,
+      @required this.accentColor,
+      @required this.heading,
+      @required this.width,
+      @required this.hintext,
+      this.maxLines});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Sans(heading, 16.0),
+        SizedBox(height: 5.0),
+        SizedBox(
+          width: width,
+          child: TextFormField(
+            inputFormatters: [
+              LengthLimitingTextInputFormatter(10),
+              FilteringTextInputFormatter.allow(RegExp('[a-zA-Z]')),
+            ],
+            maxLines: maxLines == null ? null : maxLines,
+            decoration: InputDecoration(
+              focusedErrorBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.red),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10.0),
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: accentColor),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10.0),
+                ),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: accentColor, width: 2.0),
+                borderRadius: BorderRadius.all(Radius.circular(15.0)),
+              ),
+              hintText: hintext,
+              hintStyle:
+                  GoogleFonts.poppins(fontSize: 14.0, color: Colors.grey),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty('accentColor', accentColor));
+  }
+}
+
+class AnimatedCardWeb extends StatefulWidget {
+  final imagePath;
+  final text;
+  final subtext;
+  final fit;
+  final reverse;
+  final accentColor;
+
+  const AnimatedCardWeb({
+    super.key,
+    @required this.imagePath,
+    @required this.text,
+    this.subtext,
+    this.fit,
+    this.reverse,
+    @required this.accentColor,
+  });
+
+  @override
+  State<AnimatedCardWeb> createState() => _AnimatedCardWebState();
+}
+
+class _AnimatedCardWebState extends State<AnimatedCardWeb>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller = AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 4),
+  )..repeat(reverse: true);
+
+  late Animation<Offset> _animation = Tween(
+    begin: widget.reverse == true ? Offset(0.0, 0.08) : Offset.zero,
+    end: widget.reverse == true ? Offset.zero : Offset(0.0, 0.08),
+  ).animate(_controller);
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+      position: _animation,
+      child: Card(
+        elevation: 30,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+          side: BorderSide(color: widget.accentColor),
+        ),
+        shadowColor: widget.accentColor,
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(
+                widget.imagePath,
+                height: 200.0,
+                width: 200.0,
+                fit: widget.fit ?? null,
+              ),
+              SizedBox(height: 10.0),
+              SansBold(widget.text, 15.0),
+              SansBold(widget.subtext, 12.0),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/web/landing_page_web.dart
+++ lib/web/landing_page_web.dart
@@ -14,6 +14,7 @@ class _LandingPageWebState extends State<LandingPageWeb> {
   @override
   Widget build(BuildContext context) {
     var heightDevice = MediaQuery.of(context).size.height;
+    var widthDevice = MediaQuery.of(context).size.width;
     return Scaffold(
       drawer: Drawer(),
       backgroundColor: Colors.white,
@@ -46,7 +47,7 @@ class _LandingPageWebState extends State<LandingPageWeb> {
             FirstSection Introduction (Photo and contacts)
            */
           Container(
-            height: heightDevice - 56,
+            height: heightDevice - 56.0,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
@@ -58,53 +59,53 @@ class _LandingPageWebState extends State<LandingPageWeb> {
                       decoration: BoxDecoration(
                         color: accentColor,
                         borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          topRight: Radius.circular(20),
-                          bottomRight: Radius.circular(20),
+                          topLeft: Radius.circular(20.0),
+                          topRight: Radius.circular(20.0),
+                          bottomRight: Radius.circular(20.0),
                         ),
                       ),
-                      padding:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                      child: SansBold("Hello I'm", 15),
+                      padding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
+                      child: SansBold("Hello I'm", 15.0),
                     ),
-                    SizedBox(height: 15),
-                    SansBold("Claes Erik", 55),
-                    Sans("Flutter developer", 30),
-                    SizedBox(height: 15),
+                    SizedBox(height: 15.0),
+                    SansBold("Claes Erik", 55.0),
+                    Sans("Flutter developer", 30.0),
+                    SizedBox(height: 15.0),
                     Row(
                       children: [
                         Icon(Icons.email),
-                        SizedBox(width: 20),
-                        Sans("claeserikjr@gmail.com", 15)
+                        SizedBox(width: 20.0),
+                        Sans("claeserikjr@gmail.com", 15.0)
                       ],
                     ),
-                    SizedBox(height: 10),
+                    SizedBox(height: 10.0),
                     Row(
                       children: [
                         Icon(Icons.call),
-                        SizedBox(width: 20),
-                        Sans("+55 85 98113-4321", 15)
+                        SizedBox(width: 20.0),
+                        Sans("+55 85 98113-4321", 15.0)
                       ],
                     ),
-                    SizedBox(height: 10),
+                    SizedBox(height: 10.0),
                     Row(
                       children: [
                         Icon(Icons.location_pin),
-                        SizedBox(width: 20),
-                        Sans("Praia Grande, SP - Brazil", 15)
+                        SizedBox(width: 20.0),
+                        Sans("Praia Grande, SP - Brazil", 15.0)
                       ],
                     ),
                   ],
                 ),
                 CircleAvatar(
-                  radius: 147.0,
+                  radius: 187.0,
                   backgroundColor: accentColor,
                   child: CircleAvatar(
-                    radius: 143.0,
+                    radius: 183.0,
                     backgroundColor: Colors.black,
                     child: CircleAvatar(
                       backgroundColor: Colors.white,
-                      radius: 140,
+                      radius: 180.0,
                       backgroundImage:
                           AssetImage("assets/Claes_foto_3_por_4-circle.png"),
                     ),
@@ -124,22 +125,22 @@ class _LandingPageWebState extends State<LandingPageWeb> {
               children: [
                 Image.asset(
                   "assets/web.jpg",
-                  height: heightDevice / 1.7,
+                  height: widthDevice / 1.9,
                 ),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    SansBold("About me", 40),
-                    SizedBox(height: 15),
+                    SansBold("About me", 40.0),
+                    SizedBox(height: 15.0),
                     Sans(
                         "Hello! I'm Claes Erik, Flutter, React Native and Android developer",
                         15),
                     Sans(
                         "I strive to ensure astounding performance with state of ",
                         15),
-                    Sans("The art security for Android, IOS and Web ", 15),
-                    SizedBox(height: 10),
+                    Sans("The art security for Android, IOS and Web ", 15.0),
+                    SizedBox(height: 10.0),
                     Row(
                       children: [
                         Container(
@@ -151,7 +152,7 @@ class _LandingPageWebState extends State<LandingPageWeb> {
                             borderRadius: BorderRadius.circular(5.0),
                           ),
                           padding: EdgeInsets.all(7.0),
-                          child: Sans("Flutter", 15),
+                          child: Sans("Flutter", 15.0),
                         ),
                         SizedBox(width: 7.0),
                         Container(
@@ -163,7 +164,7 @@ class _LandingPageWebState extends State<LandingPageWeb> {
                             borderRadius: BorderRadius.circular(5.0),
                           ),
                           padding: EdgeInsets.all(7.0),
-                          child: Sans("Firebase", 15),
+                          child: Sans("Android", 15.0),
                         ),
                         SizedBox(width: 7.0),
                         Container(
@@ -175,7 +176,7 @@ class _LandingPageWebState extends State<LandingPageWeb> {
                             borderRadius: BorderRadius.circular(5.0),
                           ),
                           padding: EdgeInsets.all(7.0),
-                          child: Sans("Android", 15),
+                          child: Sans("Web", 15.0),
                         ),
                         SizedBox(width: 7.0),
                         Container(
@@ -187,7 +188,19 @@ class _LandingPageWebState extends State<LandingPageWeb> {
                             borderRadius: BorderRadius.circular(5.0),
                           ),
                           padding: EdgeInsets.all(7.0),
-                          child: Sans("Web", 15),
+                          child: Sans("Back-end", 15.0),
+                        ),
+                        SizedBox(width: 7.0),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                                color: accentColor,
+                                style: BorderStyle.solid,
+                                width: 2.0),
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          padding: EdgeInsets.all(7.0),
+                          child: Sans("Firebase", 15.0),
                         ),
                       ],
                     )
@@ -197,30 +210,102 @@ class _LandingPageWebState extends State<LandingPageWeb> {
             ),
           ),
           /**
-           * ThirdSection About Me and Skills
+           * ThirdSection What i do
            */
           Container(
             height: heightDevice / 1.3,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                SansBold("What I do", 40),
+                SansBold("What I do", 40.0),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Card(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Image.asset(
-                            "assets/webL.png",
-                            height: 200.0,
-                            width: 200.0,
-                          ),
-                        ],
-                      ),
-                    ),
+                    AnimatedCardWeb(
+                        imagePath: "assets/webL.png",
+                        text: "Web Development",
+                        subtext: "(React and Flutter)",
+                        accentColor: accentColor),
+                    AnimatedCardWeb(
+                        imagePath: "assets/app.png",
+                        text: "App Development",
+                        subtext: "(Android Native and Flutter)",
+                        fit: BoxFit.contain,
+                        reverse: true,
+                        accentColor: accentColor),
+                    AnimatedCardWeb(
+                        imagePath: "assets/firebase.png",
+                        text: "Back-end Development",
+                        subtext: "(Firebase and JavaSpring)",
+                        accentColor: accentColor),
                   ],
+                )
+              ],
+            ),
+          ),
+          /**
+           * Forth Section Contact Me
+           */
+          Container(
+            height: heightDevice,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SansBold("Contact Me", 40.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        TextForm(
+                          accentColor: accentColor,
+                          width: 350.0,
+                          heading: "First Name",
+                          hintext: "Please enter your first name",
+                        ),
+                        SizedBox(height: 15.0),
+                        TextForm(
+                            accentColor: accentColor,
+                            heading: "Email",
+                            width: 350.0,
+                            hintext: "Please enter your email address")
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        TextForm(
+                          accentColor: accentColor,
+                          heading: "Last Name",
+                          width: 350.0,
+                          hintext: "Please type you last name",
+                        ),
+                        SizedBox(height: 15.0),
+                        TextForm(
+                            accentColor: accentColor,
+                            heading: "Phone Number",
+                            width: 350.0,
+                            hintext: "Please type your phone number"),
+                      ],
+                    )
+                  ],
+                ),
+                TextForm(
+                  accentColor: accentColor,
+                  heading: "Message",
+                  width: widthDevice / 1.6,
+                  hintext: "Please type your message",
+                  maxLines: 10,
+                ),
+                MaterialButton(
+                  height: 60.0,
+                  minWidth: 200.0,
+                  elevation: 20.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  color: accentColor,
+                  child: SansBold("Submit", 20.0),
+                  onPressed: () {},
                 )
               ],
             ),
