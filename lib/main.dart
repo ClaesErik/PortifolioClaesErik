e5bd844c0a4033c4fa83b325d5df4c2537687a83
Claes Erik <claeserikjr@gmail.com>
2025-01-10 16:11:17 -0300

    setup firebase to be our database

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:claes_erik/routes.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   usePathUrlStrategy();
   runApp(const MyApp());
 }
